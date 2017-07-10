38baed201ca709b8c3c9e98f2f4e85cd92de8afe
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2017-07-10 19:29:38 +0300

    Amazon wallet widget added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-chrome.runtime.sendMessage('njboiphpjlcgnbdmlohepapjkdpnpknc', location.search.substring(1));
+// documentation: https://pay.amazon.com/us/developer/documentation/automatic/201757500
+window.onAmazonPaymentsReady = () => new OffAmazonPayments.Widgets.Wallet({
+// sellerId from https://sellercentral.amazon.com/hz/me/integration/details
+  sellerId: 'A1AGEQIAP2YAIF',
+  design: {designMode: 'responsive'},
+  onReady: () => console.log(1),
+  onError: er => console.error(er)
+}).bind('walletWidgetDiv');
+
+// vim: set shiftwidth=2 tabstop=2 softtabstop=2 expandtab smarttab:
