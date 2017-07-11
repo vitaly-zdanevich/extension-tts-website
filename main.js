9567fb7206cc3f4bb4979f63684e39e2e8845eea
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2017-07-11 09:15:47 +0300

    Order reference creating

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+var orderReferenceId = null;
+
 // documentation: https://pay.amazon.com/us/developer/documentation/automatic/201757500
 window.onAmazonPaymentsReady = () => new OffAmazonPayments.Widgets.Wallet({
 // sellerId from https://sellercentral.amazon.com/hz/me/integration/details
   sellerId: 'A1AGEQIAP2YAIF',
+
+  // about order reference without AddressBook widget: https://pay.amazon.com/us/developer/documentation/lpwa/201953690
+  onOrderReferenceCreate: orderReference => orderReferenceId = orderReference.getAmazonOrderReferenceId(),
+
   design: {designMode: 'responsive'},
   onReady: () => console.log(1),
   onError: er => console.error(er.getErrorCode(), er.getErrorMessage())
