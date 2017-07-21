05002528c0a2961d2942c39e7fc710f8250a3fdf
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2017-07-21 22:48:22 +0300

    OAuth: Facebook without js sdk

--- auth.js
+++ auth.js
@@ -1,48 +1,25 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '191546788049485',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v2.10'
-  });
-  FB.AppEvents.logPageView();
-  FB.getLoginStatus(r => {
-    if (r['status'] === 'connected') {
-      // TODO hide login button
-      let aboutUser = {
-        'userId': r['authResponse']['userID'],
-        'accessToken': r['authResponse']['accessToken']
-      };
-      FB.api('/me?fields=name,email,picture', r => {  // TODO omit Facebook js sdk to fetch()
-        let url = new URLSearchParams(window.location.search.slice(1))
-        aboutUser['clientKey'] = url.get('clientKey');
+window.onload = () => {
 
-        aboutUser['name'] = r['name'];
-        aboutUser['email'] = r['email'];
+  let urlCurrent = new URLSearchParams(window.location.search.slice(1));
 
-        // extension will download it - because have the same cookies as a browser
-        aboutUser['avatarPrivateUrl'] = r['picture']['data']['url'];
-        console.log(aboutUser);
-        fetch('https://jmf2a0t2e4.execute-api.eu-west-1.amazonaws.com/extension/auth', {
-          'method': 'POST',  // TODO GET will be faster because without OPTIONS
-          'headers': {'Content-Type': 'application/json'},
-          'body': JSON.stringify(aboutUser)
-        }).then(r => r.json()).then(r => console.log(r));
-      })
-    } else {
-      window.location = encodeURI(
-        'https://www.facebook.com/dialog/oauth?client_id=191546788049485&redirect_uri=http://localhost:8000/auth.html&response_type=token');
-    }
-  });
-};
+  let urlToSetAmazon = 'https://www.amazon.com/ap/oa?' +
+    'client_id=amzn1.application-oa2-client.e09d5b674f464312bc705d36b0bc445f' + // TODO replace to id of the app on prod account
+    '&scope=profile' +
+    '&response_type=code' +
+    '&redirect_uri=https://jmf2a0t2e4.execute-api.eu-west-1.amazonaws.com/extension/auth' + // TODO replace url to our domain
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+    // random value like 326263189424723457354115822995 - client will use it for first POST with exchanging to user_id
+    '&state=' + urlCurrent.get('state');
+  document.querySelector('#a-amazon').href = urlToSetAmazon;
 
+
+  let urlToSetFacebook = 'https://www.facebook.com/dialog/oauth?' +
+    'client_id=191546788049485' +  // TODO replace to id of the app on prod account
+    '&scope=email' +
+    '&response_type=code' +
+    '&redirect_uri=https://jmf2a0t2e4.execute-api.eu-west-1.amazonaws.com/extension/auth' +
+    '&state=' + urlCurrent.get('state');
+    document.querySelector('#a-facebook').href = urlToSetFacebook;
+}
 
 // vim: set shiftwidth=2 tabstop=2 softtabstop=2 expandtab smarttab:
